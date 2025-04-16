e47a185d21b1278753848789e97b503b6bac7a6f
Alex Miller <alex.k.miller@gmail.com>
2025-04-16 14:02:13 -0400

    Revert "Failed experiment, PNG"
    This reverts commit 5dcab32d0ab66b32d1de54f8d219335ec8eb27f4.

--- code/leg_to_md.py
+++ code/leg_to_md.py
@@ -3,55 +3,46 @@ import argparse
 from dotenv import load_dotenv
 from glob import glob
 from tqdm import tqdm
-import base64
 from mistralai import Mistral, DocumentURLChunk
 
-
-def encode_image(image_path):
-    """Encode the image to base64."""
-    try:
-        with open(image_path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode('utf-8')
-    except FileNotFoundError:
-        print(f"Error: The file {image_path} was not found.")
-        return None
-    except Exception as e:  # Added general exception handling
-        print(f"Error: {e}")
-        return None
-
-
-def mistral_ocr(client, file_name, base64_image):
-    ocr_response = client.ocr.process(
-        model="mistral-ocr-latest",
-        document={
-            "type": "image_url",
-            "image_url": f"data:image/jpeg;base64,{base64_image}" 
-        }
+def mistral_ocr(client, file_name, pdf_binary):
+    uploaded_file = client.files.upload(
+        file={
+            "file_name": file_name,
+            "content": pdf_binary,
+        },
+        purpose="ocr",
     )
+
+    signed_url = client.files.get_signed_url(file_id=uploaded_file.id, expiry=1)
+
+    pdf_response = client.ocr.process(document=DocumentURLChunk(document_url=signed_url.url), model="mistral-ocr-latest", include_image_base64=True)
+
     markdowns: list[str] = []
-    for page in ocr_response.pages:
+    for page in pdf_response.pages:
         markdowns.append(page.markdown)
 
     return "\n\n".join(markdowns)
 
 
 def main(client, session_year):
-    input_dir = os.path.abspath(f'data/{session_year}rs/png')
+    input_dir = os.path.abspath(f'data/{session_year}rs/pdf')
     output_dir = os.path.abspath(f'data/{session_year}rs/md')
     os.makedirs(output_dir, exist_ok=True)
-    pdf_wildcard = os.path.join(input_dir, '*.png')
+    pdf_wildcard = os.path.join(input_dir, '*.pdf')
     pdf_files = glob(pdf_wildcard)
     for pdf_file in tqdm(pdf_files):
         file_basename = os.path.basename(pdf_file)
-        if file_basename != "HB0548.png":
+        if file_basename != "HB0548.pdf":
             continue
+        import pdb; pdb.set_trace()
         file_name, _ = os.path.splitext(file_basename)
-        destination_basename = '{}_png.md'.format(file_name)
+        destination_basename = '{}.md'.format(file_name)
         destination_file_path = os.path.join(output_dir, destination_basename)
         if not os.path.exists(destination_file_path):
-            # Getting the base64 string
-            base64_image = encode_image(pdf_file)
-            full_text = mistral_ocr(client, file_basename, base64_image)
+            with open(pdf_file, "rb") as f:
+                pdf_binary = f.read()
+            full_text = mistral_ocr(client, file_basename, pdf_binary)
             with open(destination_file_path, 'w', encoding='utf-8') as destination_file:
                 destination_file.write(full_text)
 
